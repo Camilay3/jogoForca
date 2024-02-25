0bccd4023d7378fe94bedce49a8e81a11ab9b831
Camilay3 <camila.santiago026@gmail.com>
2024-02-25 14:10:30 -0300

    Esqueleto base

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ let palavras = [];
 let alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let acentos = ['´', '~', '^', '-'];
 
-let teclado = document.querySelector('div.botoes');
+// PALAVRA
+let tentativas = document.querySelector('div.tentativas');
+for (let x = 5; x >= 1; x--) {
+    let palavra = document.createElement('div');
+    palavra.className = 'palavra';
 
+    for (let x = 5; x >= 1; x--) {
+        let letr = document.createElement('p');
+        letr.textContent = `X`;
+        letr.className = 'letr';
+        palavra.appendChild(letr);
+    }
+
+    tentativas.appendChild(palavra);
+}
+
+// TECLADO
+let teclado = document.querySelector('div.botoes');
 for (element of alfabeto) {
     let label = document.createElement('label');
     label.setAttribute('for', element);
@@ -14,6 +30,6 @@ for (element of alfabeto) {
     input.setAttribute('name', 'letras');
     input.id = element;
     
-    teclado.appendChild(label);
     teclado.appendChild(input);
+    teclado.appendChild(label);
 }
