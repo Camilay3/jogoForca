d02ddde09c01c144677522f9f0d3b14625e22939
Camilay3 <camila.santiago026@gmail.com>
2024-02-25 14:48:49 -0300

    Script das cores

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let palavras = [];
 let alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let acentos = ['´', '~', '^', '-'];
 
+let palTeste = 'Feudo'
 // PALAVRA
 let tentativas = document.querySelector('div.tentativas');
 for (let x = 5; x >= 1; x--) {
@@ -24,6 +25,7 @@ for (element of alfabeto) {
     let label = document.createElement('label');
     label.setAttribute('for', element);
     label.textContent = element;
+    label.className = 'teclas';
     
     let input = document.createElement('input');
     input.setAttribute('type', 'radio');
@@ -33,3 +35,24 @@ for (element of alfabeto) {
     teclado.appendChild(input);
     teclado.appendChild(label);
 }
+
+let letrasTec = document.querySelectorAll('label.teclas');
+
+letrasTec.forEach((item) => {
+    item.addEventListener('click', () => {
+
+        if (item.style.backgroundColor != 'red' && item.style.backgroundColor != 'green') {
+            if (palTeste.toUpperCase().split('').includes(item.textContent)) {
+                console.log(`A letra ${item.textContent} está na palavra ${palTeste}`);
+                item.style.backgroundColor = 'green';
+
+            } else {
+                console.log(`A letra ${item.textContent} não está na palavra ${palTeste}`);
+                item.style.backgroundColor = 'red';
+            }
+            item.style.cursor = 'auto';
+            item.style.border = '2px solid cadetblue';
+        }
+
+    });
+});
